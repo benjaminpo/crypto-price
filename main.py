8f5714b0f217309a6e647304d751cdaa15d469b1
Benjamin Po <benjaminpo212@gmail.com>
2021-10-06 23:36:22 +0800

    utilize yfinance threads

--- main.py
+++ main.py
@@ -13,25 +13,32 @@ CRYPTO_LIST_PATH = CONFIG.get('CRYPTO', 'LIST_PATH')
 FILE_ENCODING = CONFIG.get('FILE', 'ENCODING')
 LOGGING_MAIN_PATH = CONFIG.get('LOGGING', 'MAIN_PATH')
 MAIN_GET_MAX_NUMBER_OF_YEAR_DATA = CONFIG.get('MAIN', 'GET_MAX_NUMBER_OF_YEAR_DATA')
-SPX_CONSTITUENTS_PATH = CONFIG.get('SPX', 'CONSTITUENTS_PATH')
 CRYPTO_PATH = CONFIG.get('CRYPTO', 'PATH')
 
 
 def fetch_cryptos_price():
     """Return nothing."""
-    today = datetime.today().strftime('%Y-%m-%d')
     pass_years = datetime.now() - relativedelta(years=int(MAIN_GET_MAX_NUMBER_OF_YEAR_DATA))
     pass_years = pass_years.strftime('%Y-%m-%d')
-
+    ticker_list = []
     with open(CRYPTO_LIST_PATH, newline='', encoding=FILE_ENCODING) as file:
         crypto_list = csv.reader(file)
         next(crypto_list)
         for crypto in crypto_list:
-            stock = crypto[0].replace(".", "-")
-            data = yf.download(stock, pass_years, today)
-            data.to_csv(CRYPTO_PATH + crypto[0] + ".csv")
-            print("Downloaded {} data".format(crypto[0]))
-            logging.info("Downloaded %s data", crypto[0])
+            ticker_list.append(crypto[0].replace(".", "-"))
+    data = yf.download(
+        tickers = ticker_list,
+        period = MAIN_GET_MAX_NUMBER_OF_YEAR_DATA + 'y',
+        interval = '1d',
+        group_by = 'ticker',
+        auto_adjust = False,
+        prepost = False,
+        threads = True,
+        proxy = None
+    )
+    data = data.T
+    for ticker in ticker_list:
+        data.loc[(ticker,),].T.to_csv(CRYPTO_PATH + ticker + '.csv', sep=',', encoding='utf-8')
 
 
 def main():
