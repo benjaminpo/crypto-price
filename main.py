24c68db80e36e96479ed0fa493a7130a0e2ea53d
Benjamin Po <benjaminpo212@gmail.com>
2022-02-03 21:06:31 +0800

    fix coding style

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import csv
 import logging
 import yfinance as yf
 
-
 CONFIG = configparser.ConfigParser()
 CONFIG.read('config/local.ini')
-CRYPTO_LIST_PATH = CONFIG.get('CRYPTO', 'LIST_PATH')
-FILE_ENCODING = CONFIG.get('FILE', 'ENCODING')
-LOGGING_MAIN_PATH = CONFIG.get('LOGGING', 'MAIN_PATH')
-MAIN_GET_MAX_NUMBER_OF_YEAR_DATA = CONFIG.get('MAIN', 'GET_MAX_NUMBER_OF_YEAR_DATA')
-CRYPTO_PATH = CONFIG.get('CRYPTO', 'PATH')
 
 
 def fetch_cryptos_price():
     """Return nothing."""
+
+    CRYPTO_LIST_PATH = CONFIG.get('CRYPTO', 'LIST_PATH')
+    FILE_ENCODING = CONFIG.get('FILE', 'ENCODING')
+    MAIN_GET_MAX_NUMBER_OF_YEAR_DATA = CONFIG.get('MAIN', 'GET_MAX_NUMBER_OF_YEAR_DATA')
+    CRYPTO_PATH = CONFIG.get('CRYPTO', 'PATH')
+
     pass_years = datetime.now() - relativedelta(years=int(MAIN_GET_MAX_NUMBER_OF_YEAR_DATA))
     pass_years = pass_years.strftime('%Y-%m-%d')
     ticker_list = []
@@ -27,21 +27,22 @@ def fetch_cryptos_price():
         for ticker in list:
             ticker_list.append(ticker[0].replace(".", "-"))
     data = yf.download(
-        tickers = ticker_list,
-        period = MAIN_GET_MAX_NUMBER_OF_YEAR_DATA + 'y',
-        interval = '1d',
-        group_by = 'ticker',
-        auto_adjust = False,
-        prepost = False,
-        threads = True,
-        proxy = None
+        tickers=ticker_list,
+        period=MAIN_GET_MAX_NUMBER_OF_YEAR_DATA + 'y',
+        interval='1d',
+        group_by='ticker',
+        auto_adjust=False,
+        prepost=False,
+        threads=True,
+        proxy=None
     )
     data = data.T
     for ticker in ticker_list:
-        data.loc[(ticker,),].T.to_csv(CRYPTO_PATH + ticker + '.csv', sep=',', encoding='utf-8')
+        data.loc[(ticker,), ].T.to_csv(CRYPTO_PATH + ticker + '.csv', sep=',', encoding='utf-8')
 
 
 def main():
+    LOGGING_MAIN_PATH = CONFIG.get('LOGGING', 'MAIN_PATH')
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s %(levelname)s %(message)s',
